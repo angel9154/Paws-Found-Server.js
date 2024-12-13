186705996f51d9f53613a97119e0de6c563a7406
angel9154 <angelgerenavazquez@outlook.com>
2024-12-13 09:57:27 -0500

    testing routes

--- routes/petsroutes.js
+++ routes/petsroutes.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const router = express.Router();
 router.use(express.json());
-const {sendMessage} = require('../controllers/pets')
+const { Pets } = require('../models');
 
 
+router.get('/', async (req, res) => {
+	// Add a message to test the route
+	const foundPets = await Pets.find();
+    res.json(foundPets);
+});
+router.post('/', async (req, res) => {
+      const createdPet = await Pets.create(req.body);
+      res.json(createdPet)
+    });
+
+    router.put('/:petId', async (req, res) => {
+
+        try {
+          const updatedPet = await Pets.findByIdAndUpdate(
+            req.params.petId, 
+            req.body,
+            {new: true})
+            res.json(updatedPet)
+        } catch (error) {
+          console.log(error)
+        }
+       
+        });
 
 
-router.post('/', sendMessage);
+        router.delete('/:petId', async (req, res) => {
+            // Add a message to test the route
+            const deletedPet = await Pets.findByIdAndDelete(req.params.petId);
+            res.json(deletedPet);
+        });
+        
 
 
 
