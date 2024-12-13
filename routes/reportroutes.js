5df61885927a3c1870c37b432c96975438918434
angel9154 <angelgerenavazquez@outlook.com>
2024-12-13 10:19:14 -0500

    testing routes 2

--- routes/reportroutes.js
+++ routes/reportroutes.js
@@ -2,7 +2,40 @@ const express = require('express');
 const router = express.Router();
 router.use(express.json());
 
+const { Report } = require('../models');
 
+
+router.get('/', async (req, res) => {
+	// Add a message to test the route
+	const foundReport = await Report.find();
+    res.json(foundReport);
+});
+router.post('/', async (req, res) => {
+      const createdReport = await Report.create(req.body);
+      res.json(createdReport)
+    });
+
+    router.put('/:reportId', async (req, res) => {
+
+        try {
+          const updatedPet = await Report.findByIdAndUpdate(
+            req.params.petId, 
+            req.body,
+            {new: true})
+            res.json(updatedPet)
+        } catch (error) {
+          console.log(error)
+        }
+       
+        });
+
+
+        router.delete('/:reportId', async (req, res) => {
+            // Add a message to test the route
+            const deletedReport = await Report.findByIdAndDelete(req.params.petId);
+            res.json(deletedReport);
+        });
+        
 
 
 
