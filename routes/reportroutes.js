2d4a14ae59fa08c51bec7c9455b4333adf18a95b
JenCW <jencortezwalters@gmail.com>
2024-12-13 08:53:59 -0800

    checked routes

--- routes/reportroutes.js
+++ routes/reportroutes.js
@@ -1,42 +1,39 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 router.use(express.json());
+const { Report } = require("../models");
 
-const { Report } = require('../models');
 
 
-router.get('/', async (req, res) => {
-	// Add a message to test the route
-	const foundReport = await Report.find();
-    res.json(foundReport);
+//routes===============================================================
+router.get("/", async (req, res) => {
+  // Add a message to test the route
+  const foundReport = await Report.find();
+  res.json(foundReport);
 });
-router.post('/', async (req, res) => {
-      const createdReport = await Report.create(req.body);
-      res.json(createdReport)
-    });
 
-    router.put('/:reportId', async (req, res) => {
+router.post("/", async (req, res) => {
+  const createdReport = await Report.create(req.body);
+  res.json(createdReport);
+});
 
-        try {
-          const updatedPet = await Report.findByIdAndUpdate(
-            req.params.petId, 
-            req.body,
-            {new: true})
-            res.json(updatedPet)
-        } catch (error) {
-          console.log(error)
-        }
-       
-        });
+router.put("/:reportId", async (req, res) => {
+  try {
+    const updatedPet = await Report.findByIdAndUpdate(
+      req.params.petId,
+      req.body,
+      { new: true }
+    );
+    res.json(updatedPet);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-
-        router.delete('/:reportId', async (req, res) => {
-            // Add a message to test the route
-            const deletedReport = await Report.findByIdAndDelete(req.params.petId);
-            res.json(deletedReport);
-        });
-        
-
-
+router.delete("/:reportId", async (req, res) => {
+  // Add a message to test the route
+  const deletedReport = await Report.findByIdAndDelete(req.params.petId);
+  res.json(deletedReport);
+});
 
 module.exports = router;
