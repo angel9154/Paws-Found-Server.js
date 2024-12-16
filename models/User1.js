91d366628af3d5da0430e70e1d4bf6ba6316427a
angel9154 <angelgerenavazquez@outlook.com>
2024-12-16 16:17:40 -0500

    updated back end

--- models/User1.js
+++ models/User1.js
@@ -7,7 +7,7 @@ const mongoose = require('mongoose');
 
 //defining the User1 Schema
 const User1Schema = new mongoose.Schema({
-  name: { type: String, required: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
 });
 
