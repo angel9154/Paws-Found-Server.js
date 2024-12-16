f3d7f2e92a4f82db70de1e986a996311179507fe
Zaki <zakiispahany@gmail.com>
2024-12-16 09:52:16 -0500

    Commiting for branch, no changes

--- models/Report.js
+++ models/Report.js
@@ -4,6 +4,9 @@ const ReportSchema = new mongoose.Schema({
   name: { type: [String], required: true },
   breed: { type: [String], required: true },
   color: { type: [String], required: true },
+  image: { type: [String], required: true},
+  lastLocation: { type: [String], required: true},
+  lastDateSeen:{ type: [String], required: true}
 
 });
 
